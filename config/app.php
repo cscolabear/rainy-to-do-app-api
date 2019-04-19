6c98a48eb2beb7688483d53735c5ca0009aa7569
Cola <cscolabear@gmail.com>
2019-04-19 16:53:54 +0800

    install graphql package and playground web gui

--- config/app.php
+++ config/app.php
@@ -209,6 +209,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'GraphQL' => Nuwave\Lighthouse\Support\Facades\GraphQLFacade::class,
     ],
 
 ];
