ba250566c848a98e0bb71c9dbf7398604932f5a1
Cola <cscolabear@gmail.com>
2019-04-19 17:01:13 +0800

    add migrate and model

---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function source()
+    {
+        return $this->belongsTo(\App\Models\Source::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(\App\Models\Category::class);
+    }
+
+}
